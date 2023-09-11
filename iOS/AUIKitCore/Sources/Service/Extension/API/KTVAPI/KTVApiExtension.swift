c5a8448a83aecb5f85488bb07666d3f9b71fd026
yiwangwuqianfjp <fan_jun_peng@163.com>
2023-09-11 17:05:26 +0800

    fix service

--- iOS/AUIKitCore/Sources/Service/Extension/API/KTVAPI/KTVApiExtension.swift
+++ iOS/AUIKitCore/Sources/Service/Extension/API/KTVAPI/KTVApiExtension.swift
@@ -12,8 +12,10 @@
     func onHighPartTime(highStartTime: Int, highEndTime: Int)
 }
 
+public typealias LyricCallback = ((String?) -> Void)
+
 /// 用户角色
-@objc public enum AUISingRole: Int {
+@objc public enum KTVSingRole: Int {
     case soloSinger = 0     //独唱者
     case coSinger           //伴唱
     case leadSinger         //主唱
