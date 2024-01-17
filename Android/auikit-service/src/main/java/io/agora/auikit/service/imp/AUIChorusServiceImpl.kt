0228a9268bd1c54b22461778f312d894f9866e74
xucz <xucom786647379@gmail.com>
2024-01-17 10:03:08 +0800

    [Android]Update chorus service with list collection and fix bugs.

--- Android/auikit-service/src/main/java/io/agora/auikit/service/imp/AUIChorusServiceImpl.kt
+++ Android/auikit-service/src/main/java/io/agora/auikit/service/imp/AUIChorusServiceImpl.kt
@@ -1,74 +1,53 @@
 package io.agora.auikit.service.imp
 
 import android.util.Log
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonObject
-import com.google.gson.ToNumberPolicy
-import com.google.gson.reflect.TypeToken
 import io.agora.auikit.model.AUIChoristerModel
 import io.agora.auikit.service.IAUIChorusService
 import io.agora.auikit.service.callback.AUICallback
 import io.agora.auikit.service.callback.AUIChoristerListCallback
 import io.agora.auikit.service.callback.AUIException
 import io.agora.auikit.service.callback.AUISwitchSingerRoleCallback
+import io.agora.auikit.service.collection.AUIListCollection
 import io.agora.auikit.service.ktv.ISwitchRoleStateListener
 import io.agora.auikit.service.ktv.KTVApi
 import io.agora.auikit.service.ktv.KTVSingRole
 import io.agora.auikit.service.ktv.SwitchRoleFailReason
-import io.agora.auikit.service.rtm.AUIRtmAttributeRespObserver
-import io.agora.auikit.service.rtm.AUIRtmException
 import io.agora.auikit.service.rtm.AUIRtmManager
-import io.agora.auikit.service.rtm.AUIRtmMessageRespObserver
-import io.agora.auikit.service.rtm.AUIRtmPlayerInfo
-import io.agora.auikit.service.rtm.AUIRtmPublishModel
-import io.agora.auikit.service.rtm.AUIRtmReceiptModel
-import io.agora.auikit.service.rtm.kAUIPlayerJoinInterface
-import io.agora.auikit.service.rtm.kAUIPlayerLeaveInterface
 import io.agora.auikit.utils.GsonTools
 import io.agora.auikit.utils.ObservableHelper
 
 const val kChorusKey = "chorus"
 
+enum class AUIChorusCmd {
+    joinChorusCmd,
+    leaveChorusCmd
+}
+
 class AUIChorusServiceImpl constructor(
     private val channelName: String,
     private val ktvApi: KTVApi,
     private val rtmManager: AUIRtmManager
-) : IAUIChorusService, AUIRtmAttributeRespObserver, AUIRtmMessageRespObserver {
+) : IAUIChorusService {
     private val TAG: String = "Chorus_LOG"
 
+    private var chorusList = mutableListOf<AUIChoristerModel>() // 合唱者列表
+
+    private val observableHelper = ObservableHelper<IAUIChorusService.AUIChorusRespObserver>()
+
+    private val listCollection = AUIListCollection(channelName, kChorusKey, rtmManager)
+
+
     init {
-        rtmManager.subscribeAttribute(channelName, kChorusKey, this)
-        rtmManager.subscribeMessage(this)
+        listCollection.subscribeWillAdd(this::metadataWillAdd)
+        listCollection.subscribeAttributesDidChanged(this::onAttributeChanged)
     }
 
     override fun deInitService(completion: AUICallback?) {
         super.deInitService(completion)
 
-        if (roomContext.getArbiter(channelName)?.isArbiter() != true) {
-            return
-        }
-        rtmManager.cleanBatchMetadata(
-            channelName,
-            remoteKeys = listOf(kChorusKey)
-        ) { error ->
-            if (error != null) {
-                completion?.onResult(AUIException(AUIException.ERROR_CODE_RTM, ""))
-            } else {
-                completion?.onResult(null)
-            }
-        }
+        listCollection.cleanMetaData(completion)
     }
 
-    private val gson: Gson = GsonBuilder()
-        .setDateFormat("yyyy-MM-dd HH:mm:ss")
-        .setObjectToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE)
-        .create()
-
-    private var chorusList = mutableListOf<AUIChoristerModel>() // 合唱者列表
-
-    private val observableHelper =
-        ObservableHelper<IAUIChorusService.AUIChorusRespObserver>()
     override fun registerRespObserver(observer: IAUIChorusService.AUIChorusRespObserver?) {
         observableHelper.subscribeEvent(observer)
     }
@@ -80,74 +59,44 @@ class AUIChorusServiceImpl constructor(
     override fun getChannelName() = channelName
 
     override fun getChoristersList(callback: AUIChoristerListCallback?) {
-        callback?.onResult(AUIException(0, ""), chorusList)
+        listCollection.getMetaData { error, value ->
+            if (error != null) {
+                callback?.onResult(error, null)
+                return@getMetaData
+            }
+
+            val list =
+                GsonTools.toList(GsonTools.beanToString(value), AUIChoristerModel::class.java)
+                    ?: mutableListOf()
+            chorusList.clear()
+            chorusList.addAll(list)
+            callback?.onResult(null, list)
+        }
     }
 
     override fun joinChorus(songCode: String?, userId: String?, callback: AUICallback?) {
-        songCode ?: return
-        userId ?: return
-
-        if (roomContext.getArbiter(channelName)?.isArbiter() == true) {
-            rtmJoinChorus(songCode, userId, callback)
-            return
-        }
-
-        val info = AUIRtmPlayerInfo(
-            songCode,
-            userId,
-            channelName
+        listCollection.addMetaData(
+            AUIChorusCmd.joinChorusCmd.name,
+            mapOf(
+                "songCode" to (songCode ?: ""),
+                "userId" to (userId ?: "")
+            ),
+            listOf(mapOf("userId" to (userId ?: ""))),
+            callback
         )
-
-        rtmManager.publishAndWaitReceipt(
-            channelName,
-            lockOwnerId,
-            AUIRtmPublishModel(
-                interfaceName = kAUIPlayerJoinInterface,
-                data = info,
-                channelName = channelName
-            )
-        ) { error ->
-            if (error != null) {
-                callback?.onResult(AUIException(AUIException.ERROR_CODE_RTM, "error=$error"))
-            } else {
-                callback?.onResult(null)
-            }
-        }
     }
 
     override fun leaveChorus(songCode: String?, userId: String?, callback: AUICallback?) {
-        songCode ?: return
-        userId ?: return
-        if (roomContext.getArbiter(channelName)?.isArbiter() == true) {
-            rtmLeaveChorus(songCode, userId, callback)
-            return
-        }
-
-        val info = AUIRtmPlayerInfo(
-            songCode,
-            userId,
-            channelName
+        listCollection.removeMetaData(
+            AUIChorusCmd.leaveChorusCmd.name,
+            listOf(mapOf("userId" to (userId ?: ""))),
+            callback
         )
-
-        rtmManager.publishAndWaitReceipt(
-            channelName,
-            lockOwnerId,
-            AUIRtmPublishModel(
-                interfaceName = kAUIPlayerLeaveInterface,
-                data = info,
-                channelName = channelName
-            )
-        ) { error ->
-            if (error != null) {
-                callback?.onResult(AUIException(AUIException.ERROR_CODE_RTM, "error=$error"))
-            } else {
-                callback?.onResult(null)
-            }
-        }
     }
 
     override fun switchSingerRole(newRole: Int, callback: AUISwitchSingerRoleCallback?) {
-        ktvApi.switchSingerRole(KTVSingRole.values().firstOrNull { it.value == newRole } ?: KTVSingRole.Audience, object :
+        ktvApi.switchSingerRole(KTVSingRole.values().firstOrNull { it.value == newRole }
+            ?: KTVSingRole.Audience, object :
             ISwitchRoleStateListener {
             override fun onSwitchRoleSuccess() {
                 callback?.onSwitchRoleSuccess()
@@ -162,34 +111,34 @@ class AUIChorusServiceImpl constructor(
     override fun cleanUserInfo(userId: String, completion: AUICallback?) {
         super.cleanUserInfo(userId, completion)
         val userList = chorusList.filter { it.userId != userId }
-        if(userList.size != chorusList.size){
+        if (userList.size != chorusList.size) {
             val metaData = mutableMapOf<String, String>()
             metaData[kChorusKey] = GsonTools.beanToString(userId) ?: ""
             rtmManager.setBatchMetadata(
                 channelName,
                 metadata = metaData
-            ){ error ->
-                if(error != null){
+            ) { error ->
+                if (error != null) {
                     completion?.onResult(AUIException(AUIException.ERROR_CODE_RTM, ""))
-                }else{
+                } else {
                     completion?.onResult(null)
                 }
             }
         }
     }
 
-    override fun onAttributeChanged(channelName: String, key: String, value: Any) {
+    private fun onAttributeChanged(channelName: String, key: String, value: Any) {
         if (key != kChorusKey) {
             return
         }
         Log.d(TAG, "channelName:$channelName,key:$key,value:$value")
 
-        val json = value as? String ?: return
+        val json = value as? String ?: GsonTools.beanToString(value) ?: return
         var chorusLists: List<AUIChoristerModel> = mutableListOf()
         if (json.contains("[")) {
-            chorusLists = gson.fromJson(json, object : TypeToken<List<AUIChoristerModel>>() {}.type) ?: mutableListOf()
+            chorusLists = GsonTools.toList(json, AUIChoristerModel::class.java) ?: mutableListOf()
         }
-        
+
         chorusLists.forEach { newChorister ->
             var hasChorister = false
             this.chorusList.forEach {
@@ -225,128 +174,12 @@ class AUIChorusServiceImpl constructor(
         }
     }
 
-    override fun onMessageReceive(channelName: String, publisherId: String, message: String) {
-        if (publisherId.isEmpty() && channelName != this.channelName) {
-            return
-        }
-
-        val publishModel: AUIRtmPublishModel<JsonObject>? =
-            GsonTools.toBean(message, object : TypeToken<AUIRtmPublishModel<JsonObject>>() {}.type)
-
-        if (publishModel?.uniqueId == null) {
-            return
-        }
-
-        if (publishModel.interfaceName == null) {
-            // receipt message from arbiter
-            val receiptModel = GsonTools.toBean(message, AUIRtmReceiptModel::class.java) ?: return
-            if (receiptModel.code == 0) {
-                // success
-                rtmManager.markReceiptFinished(receiptModel.uniqueId, null)
-            } else {
-                // failure
-                rtmManager.markReceiptFinished(
-                    receiptModel.uniqueId, AUIRtmException(
-                        receiptModel.code,
-                        receiptModel.reason, "receipt message from arbiter"
-                    )
-                )
-            }
-        } else {
-            val info =
-                GsonTools.toBean(publishModel.data, AUIRtmPlayerInfo::class.java)
-            if (info == null) {
-                rtmManager.sendReceipt(
-                    channelName,
-                    publisherId,
-                    AUIRtmReceiptModel(publishModel.uniqueId, -1, channelName, "Gson parse failed!")
-                )
-                return
-            }
-            when(publishModel.interfaceName){
-                kAUIPlayerJoinInterface -> {
-                    rtmJoinChorus(info.songCode, info.userId) { error ->
-                        rtmManager.sendReceipt(
-                            channelName,
-                            publisherId,
-                            AUIRtmReceiptModel(
-                                publishModel.uniqueId,
-                                error?.code ?: 0,
-                                channelName,
-                                error?.message ?: ""
-                            )
-                        )
-                    }
-                }
-                kAUIPlayerLeaveInterface -> {
-                    rtmLeaveChorus(info.songCode, info.userId) { error ->
-                        rtmManager.sendReceipt(
-                            channelName,
-                            publisherId,
-                            AUIRtmReceiptModel(
-                                publishModel.uniqueId,
-                                error?.code ?: 0,
-                                channelName,
-                                error?.message ?: ""
-                            )
-                        )
-                    }
-                }
-            }
-        }
-    }
-
-    private fun rtmJoinChorus(songCode: String, userId: String, callback: AUICallback?){
-        val index = chorusList.indexOfFirst { it.userId == userId }
-        if(index >= 0){
-            callback?.onResult(AUIException(AUIException.ERROR_CODE_CHORISTER_ALREADY_EXIST, ""))
-            return
-        }
-
-        val metaData = mutableMapOf<String, String>()
-        var willError: AUIException? = null
-        observableHelper.notifyEventHandlers {
-            willError = it.onWillJoinChorus(songCode, userId, metaData)
-            if (willError != null) {
-                return@notifyEventHandlers
-            }
-        }
-        if(willError != null){
-            callback?.onResult(willError)
-            return
-        }
-
-        val list = ArrayList(chorusList)
-        val model = AUIChoristerModel()
-        model.chorusSongNo = songCode
-        model.userId = userId
-        list.add(model)
-        metaData[kChorusKey] = GsonTools.beanToString(list) ?: ""
-        rtmManager.setBatchMetadata(channelName, metadata = metaData){ error ->
-            if (error == null) {
-                callback?.onResult(null)
-            } else {
-                callback?.onResult(AUIException(AUIException.ERROR_CODE_RTM, "error: $error"))
-            }
-        }
-    }
-
-    private fun rtmLeaveChorus(songCode: String, userId: String, callback: AUICallback?){
-        val list = chorusList.filter { it.userId != userId }
-        if(list.size == chorusList.size){
-            callback?.onResult(AUIException(AUIException.ERROR_CODE_CHORISTER_NOT_EXIST, ""))
-            return
-        }
-
-        val metaData = mutableMapOf<String, String>()
-        metaData[kChorusKey] = GsonTools.beanToString(list) ?: ""
-        rtmManager.setBatchMetadata(channelName, metadata = metaData){ error ->
-            if (error == null) {
-                callback?.onResult(null)
-            } else {
-                callback?.onResult(AUIException(AUIException.ERROR_CODE_RTM, "error: $error"))
-            }
-        }
+    private fun metadataWillAdd(
+        publisherId: String,
+        valueCmd: String?,
+        value: Map<String, Any>
+    ): AUIException? {
+        return null
     }
 
 }
