0a3739c50499ad75e78c6f0cf1c07787bc699392
wushengtao <tamworth@163.com>
2024-07-18 10:59:37 +0800

    fix: image reader fixed

--- iOS/AUIKitCore/Sources/Core/Utils/Extension/UIImage+AUIKit.swift
+++ iOS/AUIKitCore/Sources/Core/Utils/Extension/UIImage+AUIKit.swift
@@ -11,14 +11,9 @@ import SwiftTheme
 //public var themeResourcePaths: Set<URL> = Set()
 extension UIImage {
     public class func aui_Image(named: String) -> UIImage? {
-        for path in AUIThemeManager.shared.themeResourcePaths {
-            let filePath = path.appendingPathComponent(named).path
-            if let image = UIImage(contentsOfFile: filePath) {
-                return image
-            }
-        }
-        if let filePath = ThemeManager.currentThemePath?.URL?.appendingPathComponent(named).path {
-            return UIImage(contentsOfFile: filePath)
+        if let filePath = String.aui_imageFilePath(named: named),
+           let image = UIImage(contentsOfFile: filePath)  {
+            return image
         }
         
         print("load image not found imagefilePath: \(ThemeManager.currentThemePath?.URL?.appendingPathComponent(named).path ?? named)")
@@ -30,22 +25,29 @@ extension UIImage {
 
 extension String {
     public static func aui_imageFilePath(named: String) -> String? {
+        let scales = ["@3x", "@2x", ""]
         for path in AUIThemeManager.shared.themeResourcePaths {
-            let filePath = path.appendingPathComponent(named).path.appendPngExentionIfEmpty()
+            for scale in scales {
+                let filePath = path.appendingPathComponent(named).path.appendPngExentionIfEmpty(scale: scale)
+                if FileManager.default.fileExists(atPath: filePath) {
+                    return filePath
+                }
+            }
+        }
+        
+        for scale in scales {
+            let filePath = ThemeManager.currentThemePath?.URL?.appendingPathComponent(named).path.appendPngExentionIfEmpty(scale: scale) ?? ""
             if FileManager.default.fileExists(atPath: filePath) {
                 return filePath
             }
         }
-        if let filePath = ThemeManager.currentThemePath?.URL?.appendingPathComponent(named).path.appendPngExentionIfEmpty() {
-            return filePath
-        }
         return nil
     }
     
-    private func appendPngExentionIfEmpty() -> String{
+    private func appendPngExentionIfEmpty(scale: String = "") -> String{
         let url = URL(fileURLWithPath: self)
         if url.pathExtension.isEmpty {
-            return appending(".png")
+            return appending("\(scale).png")
         }
         return self
     }
